267abc2b8cf28bf4676ff9f47cca57943eadc3a7
오충협 <tractar0213@gmail.com>
2020-09-02 16:52:04 +0900

    Error message 추가

--- src/constants/errorConstants.js
+++ src/constants/errorConstants.js
@@ -1,7 +1,6 @@
 export const errorConstants = {
   GLOBAL_0001: '문제가 발생하였습니다.\n 관리자에게 문의하여주세요.',
 
-
   AUTH_0001: '등록되지 않은 사용자입니다.',
   AUTH_0002: '비밀번호가 잘못되었습니다.',
   AUTH_0003: '탈퇴한 회원입니다.',
@@ -10,5 +9,4 @@ export const errorConstants = {
   REGIST_0001: '중복된 username이 있습니다.',
   REGIST_0002: '비밀번호가 일치하지 않습니다.',
   REGIST_0003: '해당 email은 이미 가입되었습니다.',
-
 }
