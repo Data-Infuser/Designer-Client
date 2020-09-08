35781ad8e3f08afaa08b0c7c2d2fe74f421cfd27
오충협 <tractar0213@gmail.com>
2020-09-08 11:14:55 +0900

    dummy data를 이용한 Meta 생성 기능 추가

--- src/reducers/metaReducer.js
+++ src/reducers/metaReducer.js
@@ -14,8 +14,9 @@ export function metas(state = {
     case metaConstants.POST_SUCCESS:
       return produce(state, draft => {
         draft.loading = false;
-        draft.items.push(action.meta);
-        draft.lastIndex = action.meta.id + 1;
+        const meta = generateSampleMeta(action.meta);
+        draft.items.push(meta);
+        draft.lastIndex = meta.id + 1;
       })
     case metaConstants.POST_FAIL:
       return produce(state, draft => {
@@ -24,4 +25,77 @@ export function metas(state = {
     default:
       return state
   }
+}
+
+const generateSampleMeta = (meta) => {
+  return {
+    ...meta,
+    ...sampleMetaObj
+  }
+}
+
+const sampleMetaObj = {
+  samples: `{"items":[["5028304693","대구상동우체국","이영숙","대구광역시 수성구 수성로 82 (상동)","053-766-0031","02"],["5148302626","대구상인1동우체국","황재동","대구광역시 달서구 상인서로10길 6-4 (상인동)","053-635-0014","02"],["5148302645","대구상인동우체국","최윤희","대구광역시 달서구 송현로 47 (상인동)","053-633-0013","02"],["5038303299","대구서문우체국","서경숙","대구광역시 서구 큰장로 101 (내당동)","053-571-0418","02"],["5048303797","대구서변동우체국","김태룡","대구광역시 북구 호국로43길 17 (서변동)","053-951-9930","02"]]}`,
+  columns: [
+    {
+      id: 1,
+      originalColumnName: "BIZRNO",
+      columnName: "BIZRNO",
+      type: "int",
+      length: undefined,
+      format: undefined,
+      nullable: false,
+      searchParams: []
+    },
+    {
+      id: 2,
+      originalColumnName: "CORP_NM",
+      columnName: "CORP_NM",
+      type: "varchar",
+      length: undefined,
+      format: undefined,
+      nullable: false,
+      searchParams: []
+    },
+    {
+      id: 3,
+      originalColumnName: "RPPR_NM",
+      columnName: "RPPR_NM",
+      type: "varchar",
+      length: undefined,
+      format: undefined,
+      nullable: false,
+      searchParams: []
+    },
+    {
+      id: 4,
+      originalColumnName: "ADDR",
+      columnName: "ADDR",
+      type: "text",
+      length: undefined,
+      format: undefined,
+      nullable: false,
+      searchParams: []
+    },
+    {
+      id: 5,
+      originalColumnName: "TELNO",
+      columnName: "TELNO",
+      type: "varchar",
+      length: undefined,
+      format: undefined,
+      nullable: false,
+      searchParams: []
+    },
+    {
+      id: 6,
+      originalColumnName: "RMK_TXT",
+      columnName: "RMK_TXT",
+      type: "int",
+      length: undefined,
+      format: undefined,
+      nullable: false,
+      searchParams: []
+    }
+  ]
 }
