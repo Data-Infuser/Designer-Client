5cace4e6de73b277ff005b15b1f4ba3f16eec3b9
Insung Kim <iskim1018@gmail.com>
2020-09-09 15:24:05 +0900

    Add tooltip

--- src/views/metas/Show.js
+++ src/views/metas/Show.js
@@ -1,22 +1,43 @@
-import React from 'react';
-import { 
-  Container, Box,
-  TableContainer, TableHead, TableRow, TableCell, TableBody, Paper, Table, 
-  Button, 
-  FormControl, FormControlLabel, FormHelperText, Input, InputLabel, Checkbox, 
-} from '@material-ui/core';
-import { display } from '@material-ui/system';
-import { useParams, useHistory } from 'react-router-dom';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import { useSelector } from 'react-redux';
-import { PageTitle, SubTitle } from 'components/typos/Title';
-import BorderedTable from 'components/tables/BorderedTable';
+import React from "react";
+import {
+  Container,
+  Box,
+  Paper,
+  Tooltip,
+  Button,
+  IconButton,
+  FormControl,
+  FormControlLabel,
+  FormHelperText,
+  Input,
+  InputLabel,
+  Checkbox,
+  Select,
+  MenuItem,
+} from "@material-ui/core";
+import { display } from "@material-ui/system";
+import { useParams, useHistory } from "react-router-dom";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { useSelector } from "react-redux";
+import { PageTitle, SubTitle } from "components/typos/Title";
+import BorderedTable from "components/tables/BorderedTable";
+
+import HelpIcon from '@material-ui/icons/Help';
 
 const xsWidth = 767;
 
 const formHeaders = [
-  "원본 컬럼명", "활용자 제공 컬럼명", "컬럼 타입", 
-  "최대 길이", "날짜 형식", "빈값 허용", "검색 설정", 
+  { key: "formHeader1", name: "원본 컬럼명" },
+  {
+    key: "formHeader2",
+    name: "활용자 제공 컬럼명",
+    tooltip: "활용자에게 제공되는 컬럼 명칭입니다.",
+  },
+  { key: "formHeader3", name: "컬럼 타입" },
+  { key: "formHeader4", name: "최대 길이" },
+  { key: "formHeader5", name: "날짜 형식", tooltip: "ex) yyyy-MM-dd HH:mm:ss" },
+  { key: "formHeader6", name: "빈값 허용" },
+  { key: "formHeader7", name: "검색 설정" },
 ];
 
 const useStyles = makeStyles((theme) => ({
@@ -35,153 +56,231 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: "#fff",
     borderRight: "solid 1px #ddd",
     borderBottom: "solid 1px #ddd",
-    '&.header': {
+    "&.header": {
       textAlign: "center",
     },
   },
   formControl: {
-    width: '100%',
+    width: "100%",
   },
-  [theme.breakpoints.up('sm')]: {
+  [theme.breakpoints.up("sm")]: {
     formControl: {
-      '& .label': {
+      "& .label": {
         display: "none",
       },
-      '& .form': {
+      "& .form": {
         marginTop: 0,
       },
-      '& .helpText': {
+      "& .helpText": {
         display: "none",
-      }
+      },
     },
   },
-  [theme.breakpoints.down('sm')]: {
+  [theme.breakpoints.down("sm")]: {
     flexTable: {
       marginBottom: 16,
-      boxShadow: '0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)',
+      boxShadow:
+        "0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)",
     },
     flexRow: {
-      width: '100%',
-      '&.header': {
-        display: 'none',
-        display: 'block',
+      width: "100%",
+      "&.header": {
+        display: "none",
+        display: "block",
       },
-      '&.last': {
-        width: '50%',
-      }
-    }
+      "&.last": {
+        width: "50%",
+      },
+    },
   },
 }));
 
 export function MetaShow(props) {
   const { id } = useParams();
-  const metas = useSelector(state => state.metas.items);
-  const meta = metas.find(el => el.id == id)
+  const metas = useSelector((state) => state.metas.items);
+  const meta = metas.find((el) => el.id == id);
   const history = useHistory();
 
   const classes = useStyles();
 
-  const handleChange = (e) => {
+  const handleChange = (e) => {};
 
-  }
+  const getType = (val) => {
+    switch (val) {
+      case "int":
+        return "int";
+      case "double":
+        return "double";
+      case "varchar":
+        return "text";
+      default:
+        return "text";
+    }
+  };
 
   const onButtonClick = (e) => {
-    history.push({ pathname: `/metas/${meta.id}/service`, state: {meta: meta}})
-  }
+    history.push({
+      pathname: `/metas/${meta.id}/service`,
+      state: { meta: meta },
+    });
+  };
 
   return (
     <Container>
       <PageTitle text="데이터셋 Meta" />
-      
-      {meta &&
+
+      {meta && (
         <Box>
           <Box className="BottomGutter">
             <Box textAlign="left">
-              <SubTitle text="데이터 예시" smallText="상위 5건의 원본 데이터를 출력합니다." />
+              <SubTitle
+                text="데이터 예시"
+                smallText="상위 5건의 원본 데이터를 출력합니다."
+              />
             </Box>
             <BorderedTable
               container={Paper}
               size="small"
-              headers={['BIZRNO', 'CORP_NM', 'RPPR_NM', 'ADDR', 'TELNO', 'RMK_TXT']}
-              rows={JSON.parse(meta.samples).items} />
+              headers={[
+                "BIZRNO",
+                "CORP_NM",
+                "RPPR_NM",
+                "ADDR",
+                "TELNO",
+                "RMK_TXT",
+              ]}
+              rows={JSON.parse(meta.samples).items}
+            />
           </Box>
 
           <Box className="BottomGutter">
             <Box textAlign="left">
-              <SubTitle text="메타데이터 정의" smallText="원천 데이터의 컬럼 정보를 정의합니다." />
+              <SubTitle
+                text="메타데이터 정의"
+                smallText="원천 데이터의 컬럼 정보를 정의합니다."
+              />
             </Box>
 
             <Box className={classes.flexTableContainer}>
               <div className={classes.flexTable}>
-                {formHeaders.map(header => {
-                  return <div key={header} className={`${classes.flexRow} header`}>{header}</div>
+                {formHeaders.map((header) => {
+                  return (
+                    <div
+                      key={header.key}
+                      className={`${classes.flexRow} header`}
+                    >
+                      {header.name}
+                      {header.tooltip && (
+                        <Tooltip title={header.tooltip} placement="right">
+                          <IconButton size="small">
+                            <HelpIcon fontSize="small" />
+                          </IconButton>
+                        </Tooltip>
+                      )}
+                    </div>
+                  );
                 })}
               </div>
 
-              {meta.columns.map(row => {
-                return(
-                  <div className={classes.flexTable} key={`meta-sample-meta-row-${row.id}`}>
-                    <div className={classes.flexRow}>{row.originalColumnName}</div>
+              {meta.columns.map((row) => {
+                return (
+                  <div
+                    className={classes.flexTable}
+                    key={`meta-sample-meta-row-${row.id}`}
+                  >
+                    <div className={classes.flexRow}>
+                      {row.originalColumnName}
+                    </div>
                     <div className={classes.flexRow}>
                       <FormControl className={classes.formControl}>
-                        <InputLabel htmlFor={`meta-${row.id}-input-columnName`}
-                          className="label">
-                          {formHeaders[1]}
+                        <InputLabel
+                          htmlFor={`meta-${row.id}-input-columnName`}
+                          className="label"
+                        >
+                          {formHeaders[1].name}
                         </InputLabel>
-                        <Input id={`meta-${row.id}-input-columnName`}
+                        <Input
+                          id={`meta-${row.id}-input-columnName`}
                           className="form"
-                          value={row.columnName} />
+                          value={row.columnName}
+                        />
                         <FormHelperText className="helpText">
-                          활용자에게 제공되는 컬럼 명칭입니다.
+                          {formHeaders[1].tooltip}
                         </FormHelperText>
                       </FormControl>
                     </div>
                     <div className={classes.flexRow}>
                       <FormControl className={classes.formControl}>
-                        <InputLabel htmlFor={`meta-${row.id}-input-type`}
-                          className="label">
-                          {formHeaders[2]}
+                        <InputLabel
+                          id={`label-meta-${row.id}-input-type`}
+                          className="label"
+                        >
+                          {formHeaders[2].name}
                         </InputLabel>
-                        <Input id={`meta-${row.id}-input-type`} 
-                          value={row.type} 
+                        <Select
+                          labelId={`label-meta-${row.id}-input-type`}
+                          id={`meta-${row.id}-input-type`}
+                          value={getType(row.type)}
                           className="form"
-                          onChange={handleChange} />
+                          onChange={handleChange}
+                        >
+                          <MenuItem value={"int"}>숫자(정수)</MenuItem>
+                          <MenuItem value={"double"}>숫자(부동소수)</MenuItem>
+                          <MenuItem value={"text"}>문자</MenuItem>
+                          <MenuItem value={"datetime"}>날짜(시간포함)</MenuItem>
+                          <MenuItem value={"date"}>날짜(시간 미포함)</MenuItem>
+                          <MenuItem value={"time"}>시간(시간 미포함)</MenuItem>
+                        </Select>
                       </FormControl>
                     </div>
                     <div className={classes.flexRow}>
                       <FormControl className={classes.formControl}>
-                        <InputLabel htmlFor={`meta-${row.id}-input-length`}
-                          className="label">
-                          {formHeaders[3]}
+                        <InputLabel
+                          htmlFor={`meta-${row.id}-input-length`}
+                          className="label"
+                        >
+                          {formHeaders[3].name}
                         </InputLabel>
-                        <Input id={`meta-${row.id}-input-length`} 
+                        <Input
+                          id={`meta-${row.id}-input-length`}
                           value={row.length}
                           className="form"
-                          onChange={handleChange} />
+                          onChange={handleChange}
+                        />
                       </FormControl>
                     </div>
                     <div className={classes.flexRow}>
                       <FormControl className={classes.formControl}>
-                        <InputLabel htmlFor={`meta-${row.id}-input-format`}
-                          className="label">
-                            {formHeaders[4]}
-                          </InputLabel>
-                        <Input id={`meta-${row.id}-input-format`} 
-                          value={row.format} 
+                        <InputLabel
+                          htmlFor={`meta-${row.id}-input-format`}
+                          className="label"
+                        >
+                          {formHeaders[4].name}
+                        </InputLabel>
+                        <Input
+                          id={`meta-${row.id}-input-format`}
+                          value={row.format}
                           className="form"
-                          onChange={handleChange} 
-                          disabled={row.type !== 'date'}/>
+                          onChange={handleChange}
+                          disabled={row.type !== "date"}
+                        />
+                        <FormHelperText className="helpText">
+                          {formHeaders[4].tooltip}
+                        </FormHelperText>
                       </FormControl>
                     </div>
                     <div className={`${classes.flexRow} last`}>
-                      <FormControlLabel 
+                      <FormControlLabel
                         control={
-                          <Checkbox id={`meta-${row.id}-input-nullable`} 
-                            checked={row.nullable} 
-                            onChange={handleChange} 
-                            color="primary"/>
+                          <Checkbox
+                            id={`meta-${row.id}-input-nullable`}
+                            checked={row.nullable}
+                            onChange={handleChange}
+                            color="primary"
+                          />
                         }
-                        label="빈값 허용" />                      
+                        label="빈값 허용"
+                      />
                     </div>
                     <div className={`${classes.flexRow} last`}>
                       <FormControl className={classes.formControl}>
@@ -199,8 +298,7 @@ export function MetaShow(props) {
             </Button>
           </Box>
         </Box>
-      }
-      
+      )}
     </Container>
-  )
+  );
 }
